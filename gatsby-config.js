2f2a00ebaf2062c15416c4ff4105cad11b765600
jasonslvyia <jasonslyvia@gmail.com>
2021-02-15 07:14:26 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-189765364-1",
+      },
+    },
+    "gatsby-plugin-offline",
+  ],
+};
