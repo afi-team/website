dcd4b50efcd947f4b4fad6eda73a43507ac41208
jasonslyvia <jasonslyvia@gmail.com>
2021-02-18 10:23:41 +0800

    chore: remove cname

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       },
     },
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-offline",
+    "gatsby-plugin-remove-serviceworker",
     "gatsby-plugin-no-sourcemaps"
   ],
 };
